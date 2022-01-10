2a56a66c71ed2d57cb377a54490aff6732871eaf
Can Mingir <canmingir@gmail.com>
2022-01-09 19:17:12 -0500

    Add ToC to learn page

--- src/components/Markdown.js
+++ src/components/Markdown.js
@@ -2,7 +2,20 @@ import "highlight.js/styles/github.css";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
-import { useEffect, useState } from "react";
+import { Children, createElement, useEffect, useState } from "react";
+
+function flatten(text, child) {
+  return typeof child === "string"
+    ? text + child
+    : Children.toArray(child.props.children).reduce(flatten, text);
+}
+
+function HeadingRenderer(props) {
+  const children = Children.toArray(props.children);
+  const text = children.reduce(flatten, "");
+  const slug = text.replace(/\W/g, "-");
+  return createElement("h" + props.level, { id: slug }, props.children);
+}
 
 function Markdown({ path }) {
   const [text, setText] = useState();
@@ -19,6 +32,11 @@ function Markdown({ path }) {
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
       rehypePlugins={[rehypeHighlight]}
+      components={{
+        h1: HeadingRenderer,
+        h2: HeadingRenderer,
+        h3: HeadingRenderer,
+      }}
     >
       {text}
     </ReactMarkdown>
--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,11 +1,39 @@
 import Default from "../layouts/Default";
 import Markdown from "../components/Markdown";
+import ReactMarkdownHeading from "react-markdown-heading";
 import learnMD from "../markdowns/learn.md";
+import { Box, Grid, Paper } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function Learn() {
+  const [text, setText] = useState("");
+
+  useEffect(() => {
+    fetch(learnMD)
+      .then((res) => res.text())
+      .then((text) => {
+        setText(text);
+      });
+  }, []);
+
   return (
     <Default>
-      <Markdown path={learnMD} />
+      <Grid container>
+        <Grid item xs={9}>
+          <Markdown path={learnMD} />
+        </Grid>
+      </Grid>
+      <Paper sx={{ position: "fixed", top: 200, right: 15, width: 350 }}>
+        <Box sx={{ margin: 5 }}>
+          <ReactMarkdownHeading
+            markdown={text}
+            hyperlink={true}
+            ulClassName={"tocUl"}
+            liClassName={"tocLi"}
+            anchorClassName={"tocAnchor"}
+          />
+        </Box>
+      </Paper>
     </Default>
   );
 }
