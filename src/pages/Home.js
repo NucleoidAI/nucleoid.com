40e9a353f7ef9d6797ad6df2062923ee4e4938db
Can Mingir <canmingir@gmail.com>
2022-02-09 15:06:16 -0500

    Add under the hood section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,8 +11,10 @@ import cylinderImage from "../images/database.png";
 import databaseImage from "../images/019-database-5.png";
 import helloWorldMD from "../markdowns/hello-world.md";
 import leafImage from "../images/leaf.png";
+import logicalDiagramImage from "../images/logical-diagram.png";
 import { makeStyles } from "@mui/styles";
 import networkingImage from "../images/networking.png";
+import nucleoidImage from "../images/113933331.png";
 import processingImage from "../images/processing.png";
 import promptImage from "../images/80553592.png";
 import transactionImage from "../images/cpu.png";
@@ -20,6 +22,7 @@ import {
   Box,
   Button,
   Container,
+  Divider,
   Fab,
   Grid,
   Paper,
@@ -56,6 +59,7 @@ function Feature({ image, primary, secondary, justify, xs, lg }) {
   return (
     <Grid
       container
+      item
       xs={12}
       lg={6}
       alignContent={"center"}
@@ -337,6 +341,66 @@ function Home() {
             />
             <Feature image={transactionImage} justify={"start"} lg />
           </Grid>
+        </Grid>
+        <Space />
+        <Divider />
+        <Space />
+        <Grid container item justifyContent={"center"}>
+          <Box maxWidth={600}>
+            <Typography variant={"h6"}>
+              Under the hood: Declarative Runtime Environment
+            </Typography>
+            <img
+              src={nucleoidImage}
+              alt={"Nucleoid"}
+              style={{ float: "left", margin: 10 }}
+              width={150}
+            />
+            <p>
+              The declarative runtime environment isolates a behavior definition
+              of a program from its technical instructions and executes
+              declarative statements, which represent logical intention without
+              carrying any technical details. In this paradigm, there is no
+              segregation regarding what data is or not, instead approaches how
+              data is related with others so that any type of data including
+              business rules can be added without requiring any additional
+              actions such as compiling, configuring, restarting as a result of
+              plasticity.
+            </p>
+            <img
+              src={logicalDiagramImage}
+              alt={"Logical Diagram"}
+              style={{ float: "right", margin: 10 }}
+              width={225}
+            />
+            <p>
+              As the name suggests, the runtime system runs as daemon process
+              and accepts an entry through operating system. As soon as an entry
+              received, JIT compiler compiles to runnable statements, and hand
+              it over to the call stack. Execution of the statement consists of
+              3 steps; updating state, adjusting dependency graph and storing,
+              and as a result, the statement can produce more statements in
+              order to provide logical integrity. Those produced statements will
+              be added into the call stack as well for further cycles.
+              Alternatively, a statement can return value when all logical
+              transactions completed.
+            </p>
+            <p>
+              As a part of plasticity, all types of statements including
+              business rules are considered as data so that statements can be
+              added without requiring any further actions such as recompiling,
+              restarting etc. Learn more at{" "}
+              <a href={"https://nucleoid.org/"}>nucleoid.org</a>
+            </p>
+            <Box sx={{ marginTop: 10 }}>
+              <blockquote>
+                Origin of name, Nucleoid comes from latin word nucleus-like,
+                which is the region in prokaryotic organisms and makes living
+                things, a living thing. Maybe, this makes machine a living
+                thing, like mentioned in Watson's behaviorism.
+              </blockquote>
+            </Box>
+          </Box>
         </Grid>
         <Space />
       </Container>
