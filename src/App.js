898d6fdad4116bb18b10ab3a174485657d3d379d
Can Mingir <canmingir@gmail.com>
2022-06-04 22:00:06 -0400

    Move pages to docs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import API from "./pages/API";
 import Default from "./layouts/Default";
-import GetStarted from "./pages/GetStarted";
 import Home from "./pages/Home";
-import Learn from "./pages/Learn";
 import React from "react";
-import Survey from "./pages/Survey";
 import theme from "./theme";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
@@ -17,10 +13,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Default />}>
             <Route index element={<Home />} />
-            <Route path={"/get-started"} element={<GetStarted />} />
-            <Route path={"/learn"} element={<Learn />} />
-            <Route path={"/api"} element={<API />} />
-            <Route path={"/community"} element={<Survey />} />
           </Route>
         </Routes>
       </ThemeProvider>
