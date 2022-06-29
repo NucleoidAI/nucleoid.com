43a1634558699720c619562691e8e221fd961a36
Can Mingir <canmingir@gmail.com>
2022-06-29 08:36:48 -0400

    Add image to clean code section

--- src/widgets/CleanCode.js
+++ src/widgets/CleanCode.js
@@ -1,30 +1,42 @@
 import GitHubIcon from "@mui/icons-material/GitHub";
+import capeImage from "../images/cape.png";
 import cleanCodeImage from "../images/computing.gif";
 import { Button, Grid, Typography } from "@mui/material";
 
 function CleanCode() {
   return (
     <Grid container item alignItems={"center"} justifyContent={"center"}>
-      <img src={cleanCodeImage} alt={"Clean Code"} width={250} />
+      <img
+        src={cleanCodeImage}
+        alt={"Clean Code"}
+        width={300}
+        style={{ boxShadow: "10px 10px #474949" }}
+      />
       <Grid
         container
         item
         xs={12}
-        lg={6}
+        lg={5}
         direction={"column"}
         justifyContent={"center"}
         alignItems={"center"}
       >
-        <Typography variant={"h4"} sx={{ my: 2 }} textAlign={"center"}>
+        <Typography variant={"h4"} sx={{ my: 1 }} textAlign={"center"}>
           Join us to fight <br />
           against Spaghetti Code!
         </Typography>
+        <Typography width={325} sx={{ my: 1 }}>
+          You surely witness bad code around you, we are working what is next
+          for coding!
+        </Typography>
+        <img src={capeImage} alt={"Cape"} width={75} />
         <Button
           startIcon={<GitHubIcon />}
           size={"small"}
           variant={"outlined"}
           sx={{
             p: 1,
+            m: 1,
           }}
           onClick={() =>
             window.open("https://github.com/NucleoidJS/Nucleoid", "_blank")
