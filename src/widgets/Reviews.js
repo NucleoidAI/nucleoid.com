a07e2a9614d4bc64bdb5b3aa3b2c74f9369f3ec5
Can Mingir <canmingir@gmail.com>
2023-02-09 13:35:20 -0500

    Rearrange reviews

--- src/widgets/Reviews.js
+++ src/widgets/Reviews.js
@@ -13,43 +13,38 @@ import {
 
 const list = [
   {
-    src: "/media/review1.png",
-    alt: "I just looked at Nucleoid and it looks very interesting! I really like the concept of having the DB in the runtime, directly, and the database being declarative/reactive. Definitely the way of the future!",
-  },
-  {
-    src: "/media/review2.png",
-    alt: "Forward to the future, the new revolution is growing.",
+    src: "/media/review8.png",
+    alt: "very interesting, definitely revolutionary idea :rocket: it will save tons of coding work!",
   },
   {
     src: "/media/review3.png",
     alt: "cool, it definitely looks interesting, i look forward to seeing it grow :)",
   },
   {
-    src: "/media/review4.png",
-    alt: "lookin like a cool project!",
-  },
-  {
-    src: "/media/review5.png",
-    alt: "looking to contribute very soon",
+    src: "/media/review7.png",
+    alt: "It is impressive that Nucleoid has AI that makes adjustment in the JS state.",
   },
   {
     src: "/media/review6.png",
     alt: "Interesting approach. Need to see more extensive example. @NucleoidJS",
   },
   {
-    src: "/media/review7.png",
-    alt: "It is impressive that Nucleoid has AI that makes adjustment in the JS state.",
+    src: "/media/review4.png",
+    alt: "lookin like a cool project!",
   },
   {
-    src: "/media/review8.png",
-    alt: "very interesting, definitely revolutionary idea :rocket: it will save tons of coding work!",
+    src: "/media/review2.png",
+    alt: "Forward to the future, the new revolution is growing.",
+  },
+  {
+    src: "/media/review5.png",
+    alt: "looking to contribute very soon",
+  },
+  {
+    src: "/media/review1.png",
+    alt: "I just looked at Nucleoid and it looks very interesting! I really like the concept of having the DB in the runtime, directly, and the database being declarative/reactive. Definitely the way of the future!",
   },
 ];
-
-for (let i = list.length - 1; i > 0; i--) {
-  const j = Math.floor(Math.random() * (i + 1));
-  [list[i], list[j]] = [list[j], list[i]];
-}
 
 const width = 375;
 const gap = 20;
