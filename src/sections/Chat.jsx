8473412f705e65fa659678d9eb62792950665c54
Can Mingir <canmingir@gmail.com>
2024-05-26 20:08:36 -0400

    Enable Nucleoid Chat button for mobile

--- src/sections/Chat.jsx
+++ src/sections/Chat.jsx
@@ -16,54 +16,51 @@ export default function Chat() {
         mb: 12,
       }}
     >
-      <Box>
-        <Box
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          mb: 2,
+        }}
+      >
+        {mdUp && (
+          // eslint-disable-next-line jsx-a11y/media-has-caption
+          <video
+            src="https://cdn.nucleoid.com/media/a229559c-ef13-40d2-b188-5ff2451ccbee.mp4"
+            controls
+          />
+        )}
+        {!mdUp && (
+          // eslint-disable-next-line jsx-a11y/media-has-caption
+          <video
+            width="100%"
+            height="auto"
+            src="https://cdn.nucleoid.com/media/a229559c-ef13-40d2-b188-5ff2451ccbee.mp4"
+            controls
+          />
+        )}
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Button
+          size="large"
+          variant="outlined"
+          onClick={() => {
+            window.location.assign("/ide/chat");
+          }}
           sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            mb: 2,
+            mx: 1,
+            textTransform: "none",
+            color: "white",
           }}
         >
-          {mdUp && (
-            // eslint-disable-next-line jsx-a11y/media-has-caption
-            <video
-              src="https://cdn.nucleoid.com/media/a229559c-ef13-40d2-b188-5ff2451ccbee.mp4"
-              controls
-            />
-          )}
-          {!mdUp && (
-            // eslint-disable-next-line jsx-a11y/media-has-caption
-            <video
-              width="100%"
-              height="auto"
-              src="https://cdn.nucleoid.com/media/a229559c-ef13-40d2-b188-5ff2451ccbee.mp4"
-              controls
-            />
-          )}
-        </Box>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          <Button
-            size="large"
-            variant="outlined"
-            onClick={() => {
-              window.location.assign("/ide/chat");
-            }}
-            sx={{
-              mx: 1,
-              textTransform: "none",
-              color: "white",
-              display: { xs: "none", md: "block" },
-            }}
-          >
-            Go to Nucleoid Chat
-          </Button>
-        </Box>
+          Go to Nucleoid Chat
+        </Button>
       </Box>
     </Container>
   );
